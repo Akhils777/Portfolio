fd3e9a10befcbbe7a3dd13a1b513db806c3f5b55
Akhil Pratap Singh <118816443+Akhils777@users.noreply.github.com>
2024-04-07 23:09:22 +0530

    Added index.html

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Software Engineer","Frontend Developer" , "Web Developer"],
+    typeSpeed: 80,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true
+});
